a89895ca984d6ee6767cb53d717bfbdf44804d0c
sarahwonago <sarahshaaleh@gmail.com>
2025-08-28 13:00:31 +0300

    Cache Invalidation Using Signals

--- properties/apps.py
+++ properties/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class PropertiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'properties'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "properties"
+
+    def ready(self):
+        # Import signals so they get registered
+        import properties.signals
